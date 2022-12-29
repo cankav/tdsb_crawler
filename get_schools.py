4c27bfdd0aff649e2a22977d4455cd9b740448f1
Can Kavaklioglu <can.kavaklioglu@gmail.com>
2022-12-29 12:52:42 -0600

    collected first set

--- get_schools.py
+++ get_schools.py
@@ -5,16 +5,31 @@ import pandas as pd
 import sys
 from selenium.common.exceptions import NoSuchElementException
 
-df = pd.DataFrame(columns=['url', 'code', 'name', 'total_students', 'JK_GR3', 'GR4_GR6', 'GR7_GR8', 'female', 'male', 'primary_not_english', 'living_in_canada_less_than_2_years', 'living_in_canada_for_3_5_years'])
+df = pd.DataFrame(columns=['url', 'code', 'name', 'total_language_data_available', 'total_students', 'JK_GR3', 'GR4_GR6', 'GR7_GR8', 'female', 'male', 'primary_not_english', 'living_in_canada_less_than_2_years', 'living_in_canada_for_3_5_years'])
 
 driver = Chrome('./chromedriver')
 driver.get('https://www.tdsb.on.ca/Find-your/School/By-School-Name/Elementary')
 
-letter = driver.find_element(By.ID, 'dnn_ctr1678_SchoolSearch_rptLetterNav_lnkLetter_1')
-letter.click()
+def scroll_to_bottom():
+    SCROLL_PAUSE_TIME = 5
+    # from https://stackoverflow.com/a/27760083/1056345
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
 
 all_schools = driver.find_elements(By.CSS_SELECTOR, '[class="schoolList"]')
-for school in all_schools:
+for school_index, school in enumerate(all_schools):
+    print('\n\nschool_index', school_index, school.text)
     link = school.find_element(By.TAG_NAME, 'a')
     url = link.get_attribute('href')
     name = link.text
@@ -31,35 +46,56 @@ for school in all_schools:
             pd.DataFrame.from_dict(row, orient='index').T
         ],
         ignore_index=True)
-    break
 
-def get_element_by_id(driver, element_id):
+print(df)
+print(df.shape)
+
+def get_element_by_id(driver, element_text):
 
     # 1) find line number of given text
     # 2) look up value by id using dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_+line_number
     try:
-        val = driver.find_element(By.ID, element_id).text
-        print(element_id, val)
-        return val
+        xpath_str = f"//strong[contains(text(),'{element_text}')]//parent::span"
+        print('\n\nxpath_str', xpath_str)
+        row_title_id = driver.find_element(By.XPATH, xpath_str).get_attribute('id')
+
     except NoSuchElementException:
-        print(element_id, 'not found')
-        return None
+        try:
+            print(element_text, 'not found, trying without strong')
+            xpath_str = f"//span[contains(text(),'{element_text}')]"
+            print('xpath_str', xpath_str)
+            row_title_id = driver.find_element(By.XPATH, xpath_str).get_attribute('id')
+
+        except NoSuchElementException:
+            print(element_text, 'not found, giving up')
+            return None
+
+
+    row_title_numeric_id = int(row_title_id.split('_')[-1])
+    val = driver.find_element(By.ID, f"dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_{str(row_title_numeric_id)}").text
+    print('found', element_text, val)
+    return val
+
+        
     
 for index, row in df.iterrows():
-    print('here',row)
+    print('index', index, 'out of', df.shape[0])
     driver.get(f"https://www.tdsb.on.ca/Find-your/Schools/Facts-and-Figures/schno/{row.code}")
-    df.loc[index, 'total_students'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_2')
-    df.loc[index, 'JK_GR3'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsLeft_3')
-    df.loc[index, 'GR4_GR6'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_4')
-    df.loc[index, 'GR7_GR8'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_5')
-    df.loc[index, 'female'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_7')
-    df.loc[index, 'male'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_8')
-    df.loc[index, 'primary_not_english'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_11')
-    df.loc[index, 'living_in_canada_less_than_2_years'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_14')
-    df.loc[index, 'living_in_canada_for_3_5_years'] = get_element_by_id( driver,  'dnn_ctr2930_ViewSPC_ctl00_rptFacts_lblFactsRight_15')
+    df.loc[index, 'total_students'] = get_element_by_id( driver,  'Total number of students')
+    df.loc[index, 'total_language_data_available'] = get_element_by_id( driver,  'Total language data available')
+    df.loc[index, 'JK_GR3'] = get_element_by_id( driver,  'Junior Kindergarten - Grade 3')
+    df.loc[index, 'GR4_GR6'] = get_element_by_id( driver,  'Grade 4 - Grade 6')
+    df.loc[index, 'GR7_GR8'] = get_element_by_id( driver,  'Grade 7 - Grade 8')
+    df.loc[index, 'female'] = get_element_by_id( driver,  'Female')
+    df.loc[index, 'male'] = get_element_by_id( driver,  'Male')
+    df.loc[index, 'primary_not_english'] = get_element_by_id( driver,  'Primary language other than English')
+    df.loc[index, 'living_in_canada_less_than_2_years'] = get_element_by_id( driver,  'Students living in Canada for 2 years or less')
+    df.loc[index, 'living_in_canada_for_3_5_years'] = get_element_by_id( driver,  'Students living in Canada for 3 - 5 years')
+    time.sleep(1)
 
 print('final df')    
 print(df.T.to_string())
 
-time.sleep(10)
+df.to_csv(f"school_data0.csv", index=False)
+
 driver.close()
